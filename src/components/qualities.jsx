744b2f8b8cebdcb041cb928d0774f758bdcff4cc
Oksana Gerasimenko <gerrus155@gmail.com>
2022-04-03 15:58:23 +0300

    splitting the users into other components

---
+++ src/components/qualities.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Qualities = (props) => {
+    const qualities = props.qualities;
+
+    const getQualityClasses = (color) => "badge m-1 p-1 bg-"+color;
+
+    return (
+        <>
+            { qualities.map(quality =>
+                <span className={getQualityClasses(quality.color)} key={quality._id}>{quality.name}</span>
+            )}
+        </>
+    )
+}
+
+export default Qualities;
